b6bf85353e7d11c230b024c6f20cc63536fc6edd
unknown <lateefahbaba123@gmail.com>
2024-02-04 22:19:39 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import First from "./components/First";
+const App=()=>{
+ return(
+    <diV><h1>hello</h1>
+    <First ></First>
+    </diV>
+ )
+}
+
+export default App;
---
+++ src/components/First.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const First=()=>{
+
+
+    return(
+        <h2>hi here my name is lateef ah and i am from kashmir jandk here i am going ot buy some chips</h2>
+    )
+}
+
+
+export default First;
